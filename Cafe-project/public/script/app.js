2b57c380e509364e7f4c86bef343dd4855f00efa
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-07-13 12:03:56 +0330

    add header mobile

--- Cafe-project/public/script/app.js
+++ Cafe-project/public/script/app.js
@@ -1,11 +1,21 @@
 const themeBtn = document.querySelector(".themeBtn")
-
+const themeBtnMb = document.querySelector(".themeBtnMb")
 var theme = localStorage.getItem("theme")
+
 if (theme == "dark") {
     document.documentElement.dataset.theme = "dark"
 
 }
+
 themeBtn.addEventListener("click", () => {
+    chageTheme()
+})
+
+themeBtnMb.addEventListener("click", () => {
+    chageTheme()
+})
+
+const chageTheme = () => {
     if (document.documentElement.dataset.theme == "light") {
         document.documentElement.dataset.theme = "dark"
         localStorage.setItem("theme", "dark")
@@ -13,4 +23,4 @@ themeBtn.addEventListener("click", () => {
         document.documentElement.dataset.theme = "light"
         localStorage.setItem("theme", "light")
     }
-})
+}
