1343cdfbb22e9653e09dabc7af2d441d2ef7045f
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2025-07-15 12:51:13 +0330

    styles menu mobile

--- Cafe-project/public/script/app.js
+++ Cafe-project/public/script/app.js
@@ -24,3 +24,33 @@ const chageTheme = () => {
         localStorage.setItem("theme", "light")
     }
 }
+const submenuBtn = document.querySelector(".submenuBtn")
+const subMenu = document.querySelector(".subMenu")
+submenuBtn.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.classList.toggle("text-orange-300")
+    subMenu.classList.toggle("subMenu--open")
+})
+
+const menuIcon = document.querySelector(".menu-icon")
+const menu = document.querySelector(".menu")
+const overlay = document.querySelector(".overlay")
+const closeMenu = document.querySelector(".closeMenu")
+
+menuIcon.addEventListener("click", () => {
+    menuMange()
+})
+
+closeMenu.addEventListener("click", () => {
+    menuMange()
+})
+
+overlay.addEventListener("click", () => {
+    menuMange()
+})
+
+const menuMange = () => {
+    menu.classList.toggle("-right-64")
+    menu.classList.toggle("right-0")
+    overlay.classList.toggle("hidden")
+    overlay.classList.toggle("flex")
+}
